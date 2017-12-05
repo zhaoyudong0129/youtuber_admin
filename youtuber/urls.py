714ec2251f8903bf1b9eae20de1b9ab432981505
zhao <zhaoyudong_0129@126.com>
2017-12-05 13:24:01 +0800

    add static to url

--- youtuber/urls.py
+++ youtuber/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from youtuber import settings
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
